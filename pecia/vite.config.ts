ec59bb4a37a358b8ae4db825a07a8a1b634bb21d
Alex Flach <af196@student.london.ac.uk>
2023-08-12 11:09:34 +0100

    updated config to register SW

--- pecia/vite.config.ts
+++ pecia/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             srcDir: 'src',
             filename: 'sw.ts',
             strategies: 'injectManifest',
-            injectRegister: false,
+            injectRegister: 'inline',
             manifest: false,
             injectManifest: { injectionPoint: null },
             devOptions: { enabled: true },
