c15595dee251b9731a9e6f54454a90a9e3628b41
Alex Flach <af196@student.london.ac.uk>
2023-08-27 09:11:43 +0100

    ux improvements

--- pecia/src/components/widgets/User/User.tsx
+++ pecia/src/components/widgets/User/User.tsx
@@ -7,22 +7,39 @@ import {
     CheckCircledIcon,
     ExclamationTriangleIcon,
     ValueIcon,
+    InfoCircledIcon,
 } from '@radix-ui/react-icons';
 
 import UserMenu from './UserMenu';
 import './User.css';
 
 const onlineSelector = (state: RootState) => state.user.online;
+const toastsSelector = (state: RootState) => state.toast;
+
+const setInfoStatus = (
+    showError: boolean,
+    showWarning: boolean,
+    toasts: number
+) => {
+    if (showError) return 'error';
+    else if (showWarning) return 'warning';
+    else return toasts ? 'info' : null;
+};
+
 const User = () => {
     const online = useSelector(onlineSelector);
+    const { showWarning, showError, toasts } = useSelector(toastsSelector);
     const dispatch = useDispatch();
 
     const [showMenu, setShowMenu] = useState(false);
+
+    const infoStatus = setInfoStatus(showError, showWarning, toasts.length);
 
     return (
         <div className="user-panel">
             <div className="info-logo">
                 <button
+                    id="info-button"
                     onClick={() => {
                         dispatch(actions.toggleToasts());
                     }}
@@ -35,6 +52,31 @@ const User = () => {
                         />
                     ) : (
                         <ValueIcon color="grey" width="25" height="25" />
+                    )}
+                    {infoStatus && (
+                        <div className="info-dot">
+                            {infoStatus === 'error' && (
+                                <ExclamationTriangleIcon
+                                    color="red"
+                                    width="15"
+                                    height="15"
+                                />
+                            )}
+                            {infoStatus === 'warning' && (
+                                <ExclamationTriangleIcon
+                                    color="orange"
+                                    width="15"
+                                    height="15"
+                                />
+                            )}
+                            {infoStatus === 'info' && (
+                                <InfoCircledIcon
+                                    color="grey"
+                                    width="15"
+                                    height="15"
+                                />
+                            )}
+                        </div>
                     )}
                 </button>
             </div>
--- pecia/src/state/slices/toast/toastSlice.ts
+++ pecia/src/state/slices/toast/toastSlice.ts
@@ -10,11 +10,23 @@ type Toast = {
 type ToastState = {
     toasts: Toast[];
     showToasts: boolean;
+    showWarning: boolean;
+    showError: boolean;
 };
 
 const addToast = {
     reducer: (state: ToastState, action: PayloadAction<Toast>) => {
         state.toasts.push(action.payload);
+        switch (action.payload.type) {
+            case 'warning':
+                state.showWarning = true;
+                break;
+            case 'error':
+                state.showError = true;
+                break;
+            default:
+                break;
+        }
     },
     prepare: (message: string, type: ToastType) => {
         return {
@@ -28,16 +40,36 @@ const addToast = {
 };
 
 const removeToast = (state: ToastState, action: PayloadAction<string>) => {
-    state.toasts = state.toasts.filter((toast) => toast.id !== action.payload);
+    const newToasts = state.toasts.filter(
+        (toast) => toast.id !== action.payload
+    );
+    state.toasts = newToasts;
+    if (!newToasts.length) state.showToasts = false;
+    if (!newToasts.find((toast) => toast.type === 'warning')) {
+        state.showWarning = false;
+    }
+    if (!newToasts.find((toast) => toast.type === 'warning')) {
+        state.showError = false;
+    }
 };
 
 const toggleToasts = (state: ToastState) => {
     state.showToasts = !state.showToasts;
 };
 
+const showWarning = (state: ToastState) => {
+    state.showWarning = true;
+};
+
+const showError = (state: ToastState) => {
+    state.showError = true;
+};
+
 const initialState: ToastState = {
     toasts: [],
     showToasts: false,
+    showWarning: false,
+    showError: false,
 };
 
 const toastSlice = createSlice({
@@ -47,6 +79,8 @@ const toastSlice = createSlice({
         addToast,
         removeToast,
         toggleToasts,
+        showError,
+        showWarning,
     },
 });
 
